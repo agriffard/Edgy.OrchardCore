4b3a7ea24cf0e146cca3d3e5a72982015f744067
Antoine Griffard <agriffard@hotmail.com>
2024-04-30 16:33:31 +0200

    Microsoft.AspNetCore.Html.Abstractions for IHtmlContent

--- src/Edgy.Core/Extensions/IHtmlContentExtensions.cs
+++ src/Edgy.Core/Extensions/IHtmlContentExtensions.cs
@@ -2,16 +2,16 @@ namespace Edgy.Core.Extensions;
 
 public static class IHtmlContentExtensions
 {
-    public static string GetString(this IHtmlContent content)
-    {
-        if (content != null)
-        {
-            using (var writer = new System.IO.StringWriter())
-            {
-                content.WriteTo(writer, HtmlEncoder.Default);
-                return writer.ToString();
-            }
-        }
-        return "";
-    }
+    //public static string GetString(this IHtmlContent content)
+    //{
+    //    if (content != null)
+    //    {
+    //        using (var writer = new System.IO.StringWriter())
+    //        {
+    //            content.WriteTo(writer, HtmlEncoder.Default);
+    //            return writer.ToString();
+    //        }
+    //    }
+    //    return "";
+    //}
 }
